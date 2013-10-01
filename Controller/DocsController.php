ed595943a73a59b770984c80701e1ed84bfc081e
jmeyo <jc@poumtchi.com>
2013-10-01 17:48:59 +0200

    Update dependcy for mopa, Add Readme Controller for DacorpBundle and global project

--- Controller/DocsController.php
+++ Controller/DocsController.php
@@ -7,9 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DocsController extends Controller
 {
     
-    public function readmeAction() {
-        $file = $this->get('kernel')->getRootDir() . "/../README.md";
-        $text = file_get_contents($file);
+    public function readMeAction() {
+        $filePath = $this->get('kernel')->getRootDir() . "/../README.md";
+        $text = file_get_contents($filePath);
+        $html = $this->container->get('markdown.parser')->transformMarkdown($text);
+        return $this->render('DacorpCoreBundle:Docs:readme.html.twig', array("readme" => $html ));
+    }
+
+    public function readMeDacorpAction() {
+        $kernel = $this->get('kernel');
+        $filePath = $kernel->locateResource('@DacorpCoreBundle/README.md');
+        $text = file_get_contents($filePath);
         $html = $this->container->get('markdown.parser')->transformMarkdown($text);
         return $this->render('DacorpCoreBundle:Docs:readme.html.twig', array("readme" => $html ));
     }
--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Dacorp Dacore Bundle
 
 Collection of services, helper, and standard view for standard services you want to use in any kind of website.
 
-It's a **Work in Progress**, do not use it yet 
+It's a **Work in Progress**, use it carefully :)
 
-TBD
+Some of the feature included :
+
+* Image Uploader Service : Integration of Punkave Image Uploader as a service tight to a generic Media Model
+* Simple README functionality included by default : give you a page with a translated version of the README.md Markdown file located in the root of your Project.
+
+
+Using the ImageUploader Service:
+
